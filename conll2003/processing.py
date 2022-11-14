98942ba2f6cf8f706404f60a68c74c9328e94467
tparanjp <tparanjp@asu.edu>
2022-11-14 15:21:44 -0700

    nltk and dataset edits

--- conll2003/processing.py
+++ conll2003/processing.py
@@ -1,6 +1,6 @@
 import csv
-with open('conll2003/eng.train') as report:
-    reader = csv.reader(report, delimiter=" ")
+with open('eng.train') as report:
+    reader = csv.reader(report, delimiter=' ')
     with open('engtrain.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for line in reader:
