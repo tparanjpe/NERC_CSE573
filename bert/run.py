9dfa30af0d58cde26dd5323260be6b644b6bd1ba
rifav <72421141+rifav@users.noreply.github.com>
2022-11-17 14:05:31 -0700

    Delete run.py

--- bert/run.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-model_checkpoint = "classtest/berttest2"
-token_classifier = pipeline(
-    "token-classification", model=model_checkpoint, aggregation_strategy="simple"
-)
-
-#use any input
-print(token_classifier("My name is Rifa and I study at Arizona State University."))
